e358bce22fc3e28e39a3a8cdb163a7bd29c790a0
Rafael Fronza <37110770+rafaelfronza@users.noreply.github.com>
2023-11-27 14:00:13 +0000

    Update validate_file.sh

--- validate_file.sh
+++ validate_file.sh
@@ -2,12 +2,13 @@
 
 VAL_DATA=${1}
 
-for line in `cat ${PWD}/known_sources`
+for line in `cat known_sources`
 do
   if [ ${VAL_DATA} == ${line} ]
   then
-    touch metadata.file
-    echo ${line} >> metadata.file
+    #touch metadata.file
+    #echo ${line} >> metadata.file
+    echo "${VAL_DATA} is on known_sources."
   else
     echo "Module not configured in known_sources."
     echo "Please contact us: THE GUYS to help you configure your repo in known_sources."
