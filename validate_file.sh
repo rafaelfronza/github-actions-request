d500fad94a559029dbdba315f34d973ff69e0249
Rafael Fronza <37110770+rafaelfronza@users.noreply.github.com>
2023-11-27 13:55:31 +0000

    Update validate_file.sh

--- validate_file.sh
+++ validate_file.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 VAL_DATA=${1}
-REPO_DATA=${2}
 
 for line in `cat known_sources`
 do
@@ -10,7 +9,7 @@ do
     touch metadata.file
     echo ${line} > metadata.file
   else
-    echo "Repo not configured in known_sources."
+    echo "Module not configured in known_sources."
     echo "Please contact us: THE GUYS to help you configure your repo in known_sources."
   fi
 done
