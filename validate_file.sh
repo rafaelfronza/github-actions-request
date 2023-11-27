14ce236d2afd9ee37d1890ce5aa957c10798792b
Rafael Fronza <37110770+rafaelfronza@users.noreply.github.com>
2023-11-27 14:08:06 +0000

    Update validate_file.sh

--- validate_file.sh
+++ validate_file.sh
@@ -9,8 +9,5 @@ do
     #touch metadata.file
     #echo ${line} >> metadata.file
     echo "${VAL_DATA} is on known_sources."
-  else
-    echo "Module not configured in known_sources."
-    echo "Please contact us: THE GUYS to help you configure your repo in known_sources."
   fi
 done
